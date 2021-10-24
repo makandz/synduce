32faba252ade704c653226c6b09786becbe8d4e9
Daniel Saunders <daniel.saunders@mail.utoronto.ca>
2021-10-24 16:28:31 -0400

    Added homepage

---
+++ frontend/src/components/HomePageBody/HomePageBody.js
@@ -0,0 +1,40 @@
+import TopLeftBlob from "../../svgs/topleftblob.svg";
+import BottomRightBlob from "../../svgs/bottomrightblob.svg";
+import "./HomePageBody.css";
+
+const HomePageBody = () => {
+  return (
+    <div className="homepage-body">
+      <div className="top-left-blob-container">
+        <img src={TopLeftBlob} className="top-left-blob"/>
+      </div>
+      <div className="bottom-right-blob-cotainer">
+        <img src={BottomRightBlob} className="bottom-right-blob"/>
+      </div>
+      <div className="synduce-typography">
+        <h1 className="synduce-branding">Synduce</h1>
+        <p className="synduce-description">
+          An open source automatic recursive function transformer
+        </p>
+      </div>
+      <div className="synduce-tryout">
+        <button className="synduce-button synduce-tryout-btn">
+          <p>Try it Online</p>
+        </button>
+        <p>
+          or view the source
+          <span>
+            <a
+              href="https://github.com/victornicolet/Synduce"
+              style={{ textDecoration: "none", color: "#00BFFF", paddingLeft: "0.25em"}}
+            >
+              on Github
+            </a>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HomePageBody;
---
+++ frontend/src/components/NavBar/NavBar.js
@@ -0,0 +1,21 @@
+import "./NavBar.css";
+import { AiOutlineArrowRight } from "react-icons/ai";
+
+const NavBar = () => {
+  return (
+    <div className="synduce-navbar">
+      <div className="synduce-navbar-brand">
+        <h1>Synduce</h1>
+      </div>
+      <div className="synduce-navbar-opts">
+        <p>Login</p>
+        <button className="synduce-button">
+          <p>Register for free</p>
+          <AiOutlineArrowRight />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
