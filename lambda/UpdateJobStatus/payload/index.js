6516823b842c84ade553528c734d39ea59b5b049
Vedang Naik <vedang.a.naik@gmail.com>
2021-10-24 16:06:17 -0400

    Parsed out jobID, etc. from SNS message for DB insertion

--- lambda/UpdateJobStatus/payload/index.js
+++ lambda/UpdateJobStatus/payload/index.js
@@ -1,3 +1,9 @@
 exports.handler = async (event) => {    
-    console.log(`Whoa, a job's done! This is what it says: ${event}`);
+    const message = JSON.parse(event['Records'][0]["Sns"]["Message"]);
+    const jobID = message["jobID"];
+    const logs = message["logs"];
+    const status = message["status"];
+
+    // TODO: Update database with these values.
+    console.log(`${jobID}: Status ${status}, logs ${logs}`);
 };
