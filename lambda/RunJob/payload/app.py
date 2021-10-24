5bc04a5be2ba6c0e267287dfbfbce4a93eefcacb
Vedang Naik <vedang.a.naik@gmail.com>
2021-10-24 13:42:30 -0400

    Reintroduced code for running Synduce, dependency on boto3

--- lambda/RunJob/payload/app.py
+++ lambda/RunJob/payload/app.py
@@ -1,37 +1,32 @@
 import boto3
 import os
+import subprocess
+import json
 
 def handler(event, context):
     print(f"Whoa, someone's dispatched a job! This is what it says: {event}")
+
+    # Extract code from SNS message.
+    message = json.loads(event['Records'][0]["Sns"]["Message"])
+    jobID = message["jobID"]
+    code = message["code"]
+    print(jobID, code)
     
+    # Write code to temporary file for Synduce to read.
+    # TODO: Forcibly terminate this if it runs for more than 14:30.
+    with open("/tmp/tmp.ml", "w") as f:
+        f.writelines(code)
+    proc = subprocess.run(["/home/opam/Synduce/_build/default/bin/Synduce.exe", "/tmp/tmp.ml"], capture_output=True)
+    print(proc.stdout)
+    
+    # Publish Synduce's output and jobID to SNS once done.
+    messagePayload = {
+        "jobID": jobID,
+        "logs": proc.stdout
+    }
     client = boto3.client('sns')
     response = client.publish(
         TopicArn=os.environ['FinishedJobsARN'],
-        Message="Hello from RunJob! This is just a test, don't worry :)",
+        Message=json.dumps(messagePayload),
     )
     print(response)
-
-
-
-# import subprocess
-# import json
-
-# def handler(event, context):
-#     print("Running Synduce...")
-
-#     text = json.loads(event["body"])["code"]
-#     print(text)
-    
-#     with open("/tmp/tmp.ml", "w") as f:
-#         f.writelines(text)
-
-#     a = subprocess.run(["/home/opam/synduce/_build/default/bin/Synduce.exe", "/tmp/tmp.ml"], capture_output=True)
-#     print(a)
-
-#     return {
-#         "statusCode": '200',
-#         "headers": {
-#             "Access-Control-Allow-Origin": "*",
-#         },
-#         "body": a.stdout
-#     }
