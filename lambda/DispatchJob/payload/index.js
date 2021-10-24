7deacb12466c4405a9c5149e8ec3e6f1775ed8ec
Vedang Naik <vedang.a.naik@gmail.com>
2021-10-24 13:41:56 -0400

    Added uuid dependency to generate jobIDs

--- lambda/DispatchJob/payload/index.js
+++ lambda/DispatchJob/payload/index.js
@@ -1,17 +1,31 @@
 const { SNSClient, PublishCommand } = require("@aws-sdk/client-sns");
 const { DynamoDBClient, BatchExecuteStatementCommand } = require("@aws-sdk/client-dynamodb");
+const { v4: uuidv4 } = require("uuid")
 
-exports.handler = async (event) => {    
-    const client = new SNSClient();
-    const command = new PublishCommand({
-      Message: "Hello from DispatchJob! This is just a test, don't worry :)",
-      TopicArn: process.env.QueuedJobsARN
-    });
-    const response = await client.send(command);
-    console.log(response);
-    
-    return {
-        statusCode: 200,
-        body: JSON.stringify('Sent message!'),
-    };
+exports.handler = async (event) => {
+  // Create jobID
+  const jobID = uuidv4();
+
+  // Message payload to send to runJob.
+  // TODO: Extract code body from http request.
+  const messagePayload = {
+    "jobID": jobID,
+    "code": ""
+  };
+  // Publish message to SNS
+  const client = new SNSClient();
+  const command = new PublishCommand({
+    Message: JSON.stringify(messagePayload),
+    TopicArn: process.env.QueuedJobsARN
+  });
+  const response = await client.send(command);
+  console.log(response);
+  
+  // Return jobID to frontend.
+  return {
+      statusCode: 200,
+      body: JSON.stringify({
+        jobID: jobID
+      })
+  };
 };
