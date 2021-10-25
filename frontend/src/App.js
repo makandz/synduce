d9b5131836643cfd48b13c1cd834e0225bbda94f
Daniel Saunders <daniel.saunders@mail.utoronto.ca>
2021-10-25 11:18:01 -0400

    Misc changes to height configuration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,10 @@ import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
-    <div style={{ display: "flex", flexFlow: "column", height: "100%" }}>
+    <>
       <NavBar />
-      <LoginPage isRegister={false}/>
-    </div>
+      <HomePage />
+    </>
   );
 }
 
