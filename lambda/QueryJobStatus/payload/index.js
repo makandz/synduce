37ab012c2eaecb04cfed8985d470a8007656143c
Vedang Naik <vedang.a.naik@gmail.com>
2021-10-27 15:40:01 -0400

    Added code to return jobID

--- lambda/QueryJobStatus/payload/index.js
+++ lambda/QueryJobStatus/payload/index.js
@@ -5,22 +5,41 @@ exports.handler = async (event) => {
   const body = JSON.parse(event["body"]);
   const jobID = body["jobID"];
 
-	// Update job in DB.
+	// Query job in DB.
 	const dbClient = new DynamoDBClient({ region: 'us-east-1' });
-    const params = {
-      Statements: [
-        {
-          Statement: "SELECT status FROM JobStatuses WHERE jobID=?",
-          Parameters: [
-            {'S': jobID}
-          ]
-        }
-      ]
-    };
+  const params = {
+    Statements: [
+      {
+        Statement: "SELECT status FROM JobStatuses WHERE jobID=?",
+        Parameters: [
+          {'S': jobID}
+        ]
+      }
+    ]
+  };
+  // Return 500 if error.
 	try {
 		const response = await dbClient.send(new BatchExecuteStatementCommand(params));
 		console.log('dbClient Response', response.Responses[0].Error);
 	} catch (err) {
 		console.error("Error:", err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify('Unable to fetch jobID status.')
+    };
 	}
+
+  // Return jobID status to frontend.
+  // TODO: Extract jobID from response.
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Headers" : "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,POST"
+    },
+    body: JSON.stringify({
+      jobID: jobID
+    })
+  };
 };
